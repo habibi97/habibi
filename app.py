00d1df20b041cfef26a9e582401330531662a0b2
habibi97 <118036272+habibi97@users.noreply.github.com>
2022-11-12 12:46:09 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+hero_name = "Habibi"
+hero_age = "25"
+old_career = "trucker"
+new_career = "programer"
+
+print(hero_name + ' is from Iraq fgeunite state in 2013. ')
+print(" He is " + hero_age +"now. ")
+print(hero_name + " struggle at first when he moved to the USA. ")
+print(" he become a " + old_career + " and work as a trucker for almost 6 years. ")
+print("after that " + hero_name + " wanted to chang his career to " + new_career)
+topic = "python"
+print(topic + " is a language used for " + new_career)
